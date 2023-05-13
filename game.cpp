8e423da7d83aab6758319eac4408b199954be3f3
dagota12 <84585431+dagota12@users.noreply.github.com>
2023-05-13 11:16:12 +0300

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<random>
+#include<cmath>
+#include<ctime>
+#include<vector>
+//maximun number of trial for players
+#define Max 5
+using namespace std;
+class Playe{
+	
+	string name;
+	int tries;
+	int max_guess;
+public:
+	int *trials=new int[100];
+	//getter and setter for 
+	//player
+};
+void menu();
+
+int main()
+{
+
+	return 0;
+}
+
+//fun declarations//
+void game(){
+	//start:quit
+	//create computer guess
+	//ask	num_of_players
+	//menu(AIguess,num_of_players)
+
+	
+	//how many number of players//
+	
+}
+//menu():
+	//1.choice
+	//fun that asks all players
+	// 	until they are out of guess
+	//2.loop
